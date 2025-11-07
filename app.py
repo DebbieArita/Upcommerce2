6b9624a718c7d7dc20aca5b7b259b5771780eb55
Debbie Mong'are <debbiearita96@gmail.com>
2025-11-07 15:46:19 +0300

    Update prometheus file

--- app.py
+++ app.py
@@ -7,27 +7,32 @@ import signal
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-  return 'Stripe Portal is up!'
+    return 'Stripe Portal is up!'
+
 
 def shutdown():
-  try:
-    time.sleep(60)
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is not None:
-      func()
-  except RuntimeError:  # Handle exception if no request context
-    print("Shutdown failed due to missing request context")
-    os.kill(os.getpid(), signal.SIGTERM)  # Alternative shutdown method
+    try:
+        time.sleep(60)
+        func = request.environ.get('werkzeug.server.shutdown')
+        if func is not None:
+            func()
+    except RuntimeError:  # Handle exception if no request context
+        print("Shutdown failed due to missing request context")
+        os.kill(os.getpid(), signal.SIGTERM)  # Alternative shutdown method
+
 
 shutdown_thread = threading.Thread(target=shutdown)
 shutdown_thread.start()
 
+
 def shutdown_on_exit():
-  shutdown_thread.join(1)  # Wait for shutdown thread to finish
+    shutdown_thread.join(1)  # Wait for shutdown thread to finish
+
 
 atexit.register(shutdown_on_exit)
 
 if __name__ == '__main__':
-  app.run(debug=True, host="0.0.0.0", port=5003)  # Listen on all interfaces, port 5000
+    app.run(debug=True, host="0.0.0.0", port=5003)  # Listen on all interfaces, port 5000
